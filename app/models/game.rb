0360fd1b364ece55c41f4370d6030dacffe2423b
ryan heupler <rheupler@gmail.com>
2016-04-14 14:22:02 -0400

    adds validation for entering initials

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :images
-
+  validates :player, length: {minimum: 1, maximum: 3}, allow_blank: false
   default_scope {order('score DESC') }
 end
