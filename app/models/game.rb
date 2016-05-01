a184fdbad6c5d7b481648bbac633f9c00720f2f2
ryan heupler <rheupler@gmail.com>
2016-05-01 00:05:48 -0400

    fixes player validation error

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :images
-  validates :player, allow_blank: false
+  validates :player, length: {minimum: 1, maximum: 10}, allow_blank: false
   default_scope {order('score DESC') }
 end
